acb72c7449de6de151c32d22460447c9a56caa10
mins8558 <mins8558@gmail.com>
2026-03-19 15:27:15 +0900

    0319

--- GamingEngine unity project/Assets/playercontroller.cs
+++ GamingEngine unity project/Assets/playercontroller.cs
@@ -5,23 +5,51 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
    public float movespeed = 7f; 
-      private Vector2 moveInput;
-      
+   private Vector2 moveInput;
+   public float jumpForce = 7f;
+   private Rigidbody2D rb;
+   private Animator myAnimator;
+
+     void Start() 
+     {
+         rb = GetComponent<Rigidbody2D>();
+         myAnimator = GetComponent<Animator>();
+         myAnimator.SetBool("move", false);
+     }
+     
+     
       public void OnMove(InputValue value)
       {
-      moveInput = value.Get<Vector2>();
+         moveInput = value.Get<Vector2>();
       }
    
+
+      public void OnJump(InputValue value)
+      {
+         if(value.isPressed)
+         {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+         }
+      }
    void Update()
    {
       if(moveInput.x > 0)
       {
          transform.localScale = new Vector3(1, 1, 1);
-      } 
+      }
+
       else if(moveInput.x < 0)
       {
          transform.localScale = new Vector3(-1, 1, 1);
       }
+
+      if(moveInput.magnitude > 0)  {
+          myAnimator.SetBool("move", true);
+      }
+      else {
+         myAnimator.SetBool("move", false);
+      }
+
       transform.Translate(Vector3.right * movespeed * moveInput.x * Time.deltaTime);
    } 
  }
